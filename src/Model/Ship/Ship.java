503191f879363dcd5916a70fe2a7be8d8ad4a967
zscrib0619 <zscrib0619@ufl.edu>
2018-02-08 18:03:29 -0500

    Added attributes/methods for Ship and ShipStats

--- src/Model/Ship/Ship.java
+++ src/Model/Ship/Ship.java
@@ -1,4 +1,56 @@
 package Model.Ship;
 
+import Model.Items.Inventory;
+import Model.Pilot.Pilot;
+import Model.Ship.ShipParts.*;
+
 public class Ship {
+
+    private Pilot myPilot;
+    private ShipStats myStats;
+    private ShipWeapon weaponSlot1;
+    private ShipWeapon weaponSlot2;
+    private ShipEngine engineSlot;
+    private ShipShield shieldSlot;
+    private ShipSpecial specialSlot;
+    private Inventory cargobay;
+    private int inventorySize;
+
+    public void equipWeapon1(ShipWeapon weapon){
+        weaponSlot1 = weapon;
+        updateStats();
+    }
+
+    public void equipWeapon2(ShipWeapon weapon){
+        weaponSlot2 = weapon;
+        updateStats();
+    }
+
+    public void equipEngine(ShipEngine engine){
+        engineSlot = engine;
+        updateStats();
+    }
+
+    public void equipShield(ShipShield shield){
+        shieldSlot = shield;
+        updateStats();
+    }
+
+    public void equipSpeical(ShipSpecial special){
+        specialSlot = special;
+        updateStats();
+    }
+
+    public void updateStats(){
+
+    }
+
+    public boolean isAlive(){
+        return myStats.isAlive();
+    }
+
+    public void takeDamage(int amount){
+        myStats.modifyCurrentHealth(amount);
+    }
+
 }
--- src/Model/Ship/ShipStats.java
+++ src/Model/Ship/ShipStats.java
@@ -1,4 +1,91 @@
 package Model.Ship;
 
 public class ShipStats {
+
+    private int maxHealth;
+    private int maxShield;
+    private int maxFuel;
+    private int maxSpeed;
+    private int currentHealth;
+    private int currentShield;
+    private int currentFuel;
+    private int currentSpeed;
+
+    //Getters and Setters
+    public void setMaxHealth(int value){
+        maxHealth = value;
+    }
+
+    public void setMaxShield(int value){
+        maxShield = value;
+    }
+
+    public void setMaxFuel(int value){
+        maxFuel = value;
+    }
+
+    public void setMaxSpeed(int value){
+        maxSpeed = value;
+    }
+
+    public int getMaxHealth(){
+        return maxHealth;
+    }
+
+    public int getMaxShield(){
+        return maxShield;
+    }
+
+    public int getMaxFuel(){
+        return maxFuel;
+    }
+
+    public int getMaxSpeed(){
+        return maxSpeed;
+    }
+
+    public void modifyCurrentHealth(int amount){
+        currentHealth += amount;
+    }
+
+    public void modifyCurrentShield(int amount){
+        currentShield += amount;
+    }
+
+    public void modifyCurrentFuel(int amount){
+        currentFuel += amount;
+    }
+
+    public void modifyCurrentSpeed(int amount){
+        currentSpeed += amount;
+    }
+
+    public int getCurrentHealth(){
+        return currentHealth;
+    }
+
+    public int getCurrentShield(){
+        return currentShield;
+    }
+
+    public int getCurrentFuel(){
+        return currentFuel;
+    }
+
+    public int getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    // End Getter/Setters
+
+    public boolean isAlive(){
+        if (currentHealth > 0){
+            return true;
+        }
+        return false;
+    }
+
+
+
+    
 }
